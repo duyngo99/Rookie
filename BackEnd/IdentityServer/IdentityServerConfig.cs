8cdbe0d74d8c4c1a00a5ead9f683bc94fb42e828
slasherdark <no4222@gmail.com>
2021-04-02 13:02:53 +0700

    Create IdentityServer4 and register the services

---
+++ BackEnd/IdentityServer/IdentityServerConfig.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace BackEnd.IdentityServer
+{
+   public static class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+             new ApiScope[]
+             {
+                  new ApiScope("rookieshop.api", "Rookie Shop API")
+             };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                // machine to machine client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // scopes that client has access to
+                    AllowedScopes = { "rookieshop.api" }
+                },
+
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44367/signin-oidc" },
+
+                    PostLogoutRedirectUris = { "https://localhost:44367/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "rookieshop.api"
+                    }
+                },
+
+                new Client
+                {
+                    ClientId = "swagger",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    RedirectUris =           { $"https://localhost:44341/swagger/oauth2-redirect.html" },
+                    PostLogoutRedirectUris = { $"https://localhost:44341/swagger/oauth2-redirect.html" },
+                    AllowedCorsOrigins =     { $"https://localhost:44341" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "rookieshop.api"
+                    }
+                },
+            };
+    }
+}
--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using BackEnd.Models;
+using BackEnd.IdentityServer;
 
 namespace BackEnd
 {
