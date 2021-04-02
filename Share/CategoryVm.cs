581a88c5ea2f5d66e6bcac97cf19e689ee23e83b
slasherdark <no4222@gmail.com>
2021-04-02 10:51:09 +0700

    Create ProductVm, CategoryVm , ProductFormVm and CategoryFormVm then reference to BackEnd MVC

---
+++ Share/CategoryVm.cs
@@ -0,0 +1,8 @@
+namespace Share
+{
+    public class CategoryVm
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Share/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Share
-{
-    public class Class1
-    {
-    }
-}
