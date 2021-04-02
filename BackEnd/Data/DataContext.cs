fd14b9e3d1e0816f592640a0c5565d3b578f7a24
slasherdark <no4222@gmail.com>
2021-04-02 10:58:55 +0700

    Create DataContext files and add migrations

---
+++ BackEnd/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
